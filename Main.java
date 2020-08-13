14169c4f7b26dfcc97588c7a3148a047f00c60d6
RaahemAhmed <69646535+RaahemAhmed@users.noreply.github.com>
2020-08-13 11:02:13 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is Raahem");
+		System.out.println("I like eating ice cream");
+		System.out.println("My favorite subject in school is math");
+		System.out.println("I enjoy playing video games and reading books");
+		System.out.println("I am aiming to go to SJSU and majoring in engineering")
+	}
+}
